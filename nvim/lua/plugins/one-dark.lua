66c8f8bf5e24ae3bf5b0e958d7e36fb85ca99983
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-01-08 20:57:22 +0100

    adjust color scheme

--- nvim/lua/plugins/one-dark.lua
+++ nvim/lua/plugins/one-dark.lua
@@ -3,7 +3,6 @@ return {
   "olimorris/onedarkpro.nvim",
   priority = 1000, -- Ensure it loads first
   config = function()
-    -- somewhere in your config:
-    vim.cmd("colorscheme onedark")
+    vim.cmd("colorscheme onedark_dark")
   end,
 }
