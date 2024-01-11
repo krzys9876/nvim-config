a70ed9b5808b1322b352b000f594a3d9d972321b
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-01-11 11:02:18 +0100

    disable which-key popup after pressing double quote to enable more fluent use of registers

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,13 +1,25 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 1000
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    triggers_nowait = {
+      -- marks
+      "`",
+      "'",
+      "g`",
+      "g'",
+      -- registers
+      -- '"' -- do not show popup when selecting register,
+      "<c-r>",
+      -- spelling
+      "z=",
+    },
+  },
 }
