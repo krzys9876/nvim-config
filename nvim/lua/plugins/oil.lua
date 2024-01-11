38727990d0f18cc01efe132305fa7aa82715467a
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-01-11 11:00:56 +0100

    add oil.lua - edit directory like a text file

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+-- edit directory like a text file
+return {
+  "stevearc/oil.nvim",
+  config = function()
+    require("oil").setup()
+    vim.keymap.set("n", "\\", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+}
